b5465e3f8f8b35a7b3e8077a6e95d45638dc4db4
combine143 <combine142@gmail.com>
2025-05-21 17:39:23 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,19 @@
 'use strict';
 
-// write code here
+const inputs = document.querySelectorAll('.field');
+const line = Array.from(inputs);
+
+const labelsText = ['First Name', 'Last Name', 'Email', 'Password', 'Email', 'Password',];
+
+line.forEach((element, index) => {
+    const input = element.querySelector('input');
+    const label = document.createElement('label');
+    label.setAttribute('for', input.id);
+    label.textContent = labelsText[index];
+    label.style.fontWeight = 'bold';
+    label.style.textTransform = 'uppercase';
+
+    input.setAttribute('placeholder', labelsText[index]);
+
+    element.insertBefore(label, input);
+});
